31a8086c6d0e927e86513ba7ca6a50ff62f4a240
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-12-27 08:54:27 +0700

    Update playlistLongestInterval solution

--- src/playlistLongestInterval/index.ts
+++ src/playlistLongestInterval/index.ts
@@ -1,45 +1,40 @@
-const toSeconds = (minutes: string, seconds: string) =>
-  +minutes * 60 + +seconds;
+function playlistLongestInterval(songs) {
+  songs = songs.map(e => e.replace(/[\(\)]/gi, ''))
+    .map(e => e.split(' '))
+    .map(e => [e.join('#'), getTime(e[1])])
 
-function playlistLongestInterval(songs: string[]) {
-  let result = 0;
-  const { length } = songs;
-  const regex = /([A-z]+)\s+\((\d{1,2}):(\d{1,2})/;
+  let max = 0;
+  let queue = [];
+  let sum = 0;
+  let set = new Set();
 
-  const formatedSongs = songs.map(song => {
-    const [, name, minutes, seconds] = song.match(regex);
-    return {
-      name,
-      minutes,
-      seconds
-    };
-  });
+  for (let i in songs) {
+    let song = songs[i][0];
+    let duration = songs[i][1];
 
-  for (let i = 0; i < length; i++) {
-    let same = undefined;
-    let lookup = {};
-    lookup[formatedSongs[i].name] = true;
-    for (let j = i + 1; j < length; j++) {
-      const currentSongName = formatedSongs[j].name;
-      if (lookup[currentSongName]) {
-        same = j;
-        break;
-      } else {
-        lookup[currentSongName] = true;
-      }
+    if (set.has(song)) {
+      max = Math.max(max, sum);
+
+      let tmp = queue.map(e => e[0])
+      let index = tmp.indexOf(song);
+      queue = queue.slice(index + 1);
+
+      set = new Set(queue.map(_ => _[0]));
+      sum = queue.map(_ => _[1]).reduce((a, b) => a + b, 0);
+
     }
 
-    const diffSongs = formatedSongs.slice(i, same);
-    let current = 0;
-
-    for (const song of diffSongs) {
-      current += toSeconds(song.minutes, song.seconds);
-    }
-
-    result = current > result ? current : result;
+    queue.push([song, duration]);
+    set.add(song);
+    sum += duration;
   }
 
-  return result;
+  return Math.max(max, sum)
 }
 
-export { playlistLongestInterval };
+function getTime(str) {
+  str = str.split(':').map(Number);
+  return str[0] * 60 + str[1];
+}
+
+export { playlistLongestInterval }
