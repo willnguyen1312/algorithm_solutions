a957e746ac3c9e6a749ec740311586be97824d45
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-01-03 10:24:07 +0700

    Ignore coverage from Jest check run

--- test/jest.testing.js
+++ test/jest.testing.js
@@ -9,6 +9,6 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts', 'tsx'],
   // explicitly transform ts/tsx with babel
   transform: {
-    '^.+\\.(tsx|js)?$': 'babel-jest',
+    '^.+\\.(ts|js)?$': 'babel-jest',
   },
 }
