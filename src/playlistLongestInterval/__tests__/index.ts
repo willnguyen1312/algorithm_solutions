a0ba1ba2adc2fb3261e76e1edcaf6573906acd77
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-01-03 11:02:09 +0700

    Config Eslint to work with Typescrip files

--- src/playlistLongestInterval/__tests__/index.ts
+++ src/playlistLongestInterval/__tests__/index.ts
@@ -1,53 +1,55 @@
-import { playlistLongestInterval } from "..";
+import { playlistLongestInterval } from '..'
 
-describe("playListLongestInterval", () => {
-  it("should work as expected", () => {
-    const input1 = ["A (1:30)", "B (2:00)", "A (1:30)", "C (1:00)"];
-    const expectedOutput1 = 270;
-    expect(playlistLongestInterval(input1)).toBe(expectedOutput1);
+describe('playListLongestInterval', () => {
+  it('should work as expected', () => {
+    const input1 = ['A (1:30)', 'B (2:00)', 'A (1:30)', 'C (1:00)']
+    const expectedOutput1 = 270
+    expect(playlistLongestInterval(input1)).toBe(expectedOutput1)
 
     const input2 = [
-      "A (1:30)",
-      "C (1:00)",
-      "C (1:00)",
-      "B (2:00)",
-      "B (2:00)",
-      "A (1:30)"
-    ];
-    const expectedOutput2 = 210;
-    expect(playlistLongestInterval(input2)).toBe(expectedOutput2);
+      'A (1:30)',
+      'C (1:00)',
+      'C (1:00)',
+      'B (2:00)',
+      'B (2:00)',
+      'A (1:30)',
+    ]
+    const expectedOutput2 = 210
+    expect(playlistLongestInterval(input2)).toBe(expectedOutput2)
 
-    const input3 = ["F (8:26)", "W (8:35)", "F (8:26)", "A (0:59)", "F (8:26)"];
-    const expectedOutput3 = 1080;
-    expect(playlistLongestInterval(input3)).toBe(expectedOutput3);
+    const input3 = ['F (8:26)', 'W (8:35)', 'F (8:26)', 'A (0:59)', 'F (8:26)']
+    const expectedOutput3 = 1080
+    expect(playlistLongestInterval(input3)).toBe(expectedOutput3)
 
-    const input4 = ["HEOIG (1:52)",
-      "F (9:24)",
-      "IXDK (0:42)",
-      "F (9:24)",
-      "D (2:11)",
-      "HEOIG (1:52)",
-      "IXDK (0:42)",
-      "GEAA (2:19)",
-      "D (2:11)",
-      "IDNQ (9:10)",
-      "VNWBLVNUEZ (0:13)",
-      "UHHZILNA (9:47)",
-      "UZVZ (5:42)",
-      "IXDK (0:42)",
-      "VNWBLVNUEZ (0:13)",
-      "LY (2:48)",
-      "UZVZ (5:42)",
-      "IDNQ (9:10)",
-      "G (3:02)",
-      "G (3:02)",
-      "IYW (4:26)",
-      "UHHZILNA (9:47)",
-      "E (4:05)",
-      "QNYZXPC (0:59)",
-      "UZVZ (5:42)"]
+    const input4 = [
+      'HEOIG (1:52)',
+      'F (9:24)',
+      'IXDK (0:42)',
+      'F (9:24)',
+      'D (2:11)',
+      'HEOIG (1:52)',
+      'IXDK (0:42)',
+      'GEAA (2:19)',
+      'D (2:11)',
+      'IDNQ (9:10)',
+      'VNWBLVNUEZ (0:13)',
+      'UHHZILNA (9:47)',
+      'UZVZ (5:42)',
+      'IXDK (0:42)',
+      'VNWBLVNUEZ (0:13)',
+      'LY (2:48)',
+      'UZVZ (5:42)',
+      'IDNQ (9:10)',
+      'G (3:02)',
+      'G (3:02)',
+      'IYW (4:26)',
+      'UHHZILNA (9:47)',
+      'E (4:05)',
+      'QNYZXPC (0:59)',
+      'UZVZ (5:42)',
+    ]
 
-    const expectedOutput4 = 1916;
-    expect(playlistLongestInterval(input4)).toBe(expectedOutput4);
-  });
-});
+    const expectedOutput4 = 1916
+    expect(playlistLongestInterval(input4)).toBe(expectedOutput4)
+  })
+})
--- src/playlistLongestInterval/index.ts
+++ src/playlistLongestInterval/index.ts
@@ -1,40 +1,40 @@
 function playlistLongestInterval(songs) {
-  songs = songs.map(e => e.replace(/[\(\)]/gi, ''))
+  songs = songs
+    .map(e => e.replace(/[\(\)]/gi, ''))
     .map(e => e.split(' '))
     .map(e => [e.join('#'), getTime(e[1])])
 
-  let max = 0;
-  let queue = [];
-  let sum = 0;
-  let set = new Set();
+  let max = 0
+  let queue = []
+  let sum = 0
+  let set = new Set()
 
   for (let i in songs) {
-    let song = songs[i][0];
-    let duration = songs[i][1];
+    let song = songs[i][0]
+    let duration = songs[i][1]
 
     if (set.has(song)) {
-      max = Math.max(max, sum);
+      max = Math.max(max, sum)
 
       let tmp = queue.map(e => e[0])
-      let index = tmp.indexOf(song);
-      queue = queue.slice(index + 1);
+      let index = tmp.indexOf(song)
+      queue = queue.slice(index + 1)
 
-      set = new Set(queue.map(_ => _[0]));
-      sum = queue.map(_ => _[1]).reduce((a, b) => a + b, 0);
-
+      set = new Set(queue.map(_ => _[0]))
+      sum = queue.map(_ => _[1]).reduce((a, b) => a + b, 0)
     }
 
-    queue.push([song, duration]);
-    set.add(song);
-    sum += duration;
+    queue.push([song, duration])
+    set.add(song)
+    sum += duration
   }
 
   return Math.max(max, sum)
 }
 
 function getTime(str) {
-  str = str.split(':').map(Number);
-  return str[0] * 60 + str[1];
+  str = str.split(':').map(Number)
+  return str[0] * 60 + str[1]
 }
 
 export { playlistLongestInterval }
